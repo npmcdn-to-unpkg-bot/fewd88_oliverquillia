22504eb3855960c173e6d920148003a208eb0189
Oliver Quillia <oquillia@gmail.com>
2016-06-27 20:29:36 -0400

    update

--- week4/first-javascript/script.js
+++ week4/first-javascript/script.js
@@ -1,5 +1,19 @@
 alert('this is an alert. this is annoying');
 
+	var fName = "Oliver";
+
 	document.write('<h2>writing to the document, like a boss</h2>');
 
 	console.log("hello");
+
+	var fname = "Oliver";
+	var lname = "Quillia";
+	var num1 = 10 * 10;
+	var num2 = 40 - 3 + 3;
+	document.write(num1 * num2);	
+
+
+
+
+
+
