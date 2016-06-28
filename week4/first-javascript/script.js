c395a0e55296743405f7f35ada8f04ab6213ad87
Oliver Quillia <oquillia@gmail.com>
2016-06-28 13:09:36 -0400

    updates

--- week4/first-javascript/script.js
+++ week4/first-javascript/script.js
@@ -1,16 +1,35 @@
 alert('this is an alert. this is annoying');
 
 	var fName = "Oliver";
-
+	alert("Oliver");
 	document.write('<h2>writing to the document, like a boss</h2>');
 
 	console.log("hello");
-
+	
 	var fname = "Oliver";
 	var lname = "Quillia";
+
+	document.write(fname + " " + lname + '<br/>');
+
+	// function definition
+
+	function doSomething(){
+			alert('click this shit godammit');
+	}
+
+	doSomething();
+
 	var num1 = 10 * 10;
 	var num2 = 40 - 3 + 3;
 	document.write(num1 * num2);	
+
+	document.getElementById('greeting').style.color = "red";
+	
+	var myPara = document.getElementById('myPara');
+	myPara.style.color = "green";
+	myPara.onclick = doSomething;
+
+
 
 
 
