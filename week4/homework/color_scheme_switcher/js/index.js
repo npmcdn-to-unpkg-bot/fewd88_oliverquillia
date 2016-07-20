cb58e562a773da728b8e288ef9bad93b90661ec2
Oliver Quillia <oquillia@gmail.com>
2016-07-20 13:08:54 -0400

    sort

---
+++ week4/homework/color_scheme_switcher/js/index.js
@@ -0,0 +1,14 @@
+alert('this sheet is working');
+
+document.getElementById('grayButton').onclick = switchGray;
+document.getElementById('whiteButton').onclick = switchWhite;
+
+function switchGray() {
+  document.body.style.backgroundColor = 'gray';
+ document.body.style.color = 'white';
+}
+
+function switchWhite() {
+  document.body.style.backgroundColor = 'white';
+  document.body.style.color = 'black';
+}
