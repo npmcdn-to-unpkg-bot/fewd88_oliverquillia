fbd405816cc52265a59aea6627ed4c6329df5527
Oliver Quillia <oquillia@gmail.com>
2016-07-25 18:34:24 -0400

    updating homework and final project

--- week6/class10/jquery-review/js/script.js
+++ week6/class10/jquery-review/js/script.js
@@ -1,5 +1,7 @@
 $('#box1').css('background-color','blue').click(function(){
+	
 	console.log('task')
+	
 	$('#box1').css('width','500px')
 	$('#box1').css('height','500px')
 
--- week6/homework/temp-converter/js/app.js
+++ week6/homework/temp-converter/js/app.js
@@ -1,28 +1,15 @@
 
-var rawTemp = "0";
-var rawUnit = "F";
-var convertedTemp = "0";
-var convertedUnit = "C";
 
-$('#submit').click(function(e){
-	rawTemp = parseInt($('#temp').val());
-	rawUnit = $("input[type='radio']:checked").val();
 
-	console.log(
-		'rawTemp: ' + rawTemp + '\n' + 
-		'rawUnit: ' + rawUnit + '\n' + 
-		'convertedTemp: ' + convertedTemp + '\n' +
-		'convertedUnit: ' + convertedUnit
-	);
 
-	if(rawUnit === 'F'){
-		convertedUnit = 'C';
-		convertedTemp = (rawTemp - 32) / 1.8;
-		$('#output').html(convertedTemp.toPrecision(3) + String.fromCharCode(176) + convertedUnit);
-	}else if(rawUnit === 'C'){
-		convertedUnit = 'F';
-		convertedTemp = (rawTemp * 1.8) + 32;
-		$('#output').text(convertedTemp.toPrecision(3) + String.fromCharCode(176) + convertedUnit);
-	}
-	e.preventDefault();
-});
+
+// var rawTemp;
+// var rawUnit;
+// var convertedTemp;
+// var convertedUnit;
+
+// $('#submit').click(function(){
+
+// 	rawTemp = parseInt($('#temp').val());
+// 	rawUnit = $("input[type='radio"]:checked").val();
+// });
