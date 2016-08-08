235d4deb9f91f7fd8a72eebcfd15e8b0903d8827
Oliver Quillia <oquillia@gmail.com>
2016-08-08 16:16:14 -0400

    updates

---
+++ Final Project 2/js/basic_slide.js
@@ -0,0 +1,11 @@
+jQuery(document).ready(function($) {
+  
+  $('#banner-slide').bjqs({
+    animtype      : 'slide',
+    height        : 320,
+    width         : 620,
+    responsive    : true,
+    randomstart   : true
+  });
+  
+});
